3dd9f3400e2359de830a565818c16a2fd59a09c3
Infinite-XD <mustafakhattak360@icloud.com>
2024-07-23 00:50:27 +0100

    Create Calculation Functions Created functions to add/subtract/divide/multiply numbers. Also made a main function that can call these functions.

---
+++ scripts.js
@@ -0,0 +1,30 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+function operate(operator, n1, n2) {
+    switch (operator) {
+        case '+':
+            return add(n1, n2)
+        case '-':
+            return subtract(n1, n2)
+        case '/':
+            return divide(n1, n2)
+        case '*':
+            return divide(n1, n2)
+    }
+}
+
+
